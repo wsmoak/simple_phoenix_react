4de3e9ec9adbcb4dc94a80feec22bd1f6c1eb08d
Wendy Smoak <wsmoak@gmail.com>
2016-09-18 10:49:38 -0400

    Reformat books data and add some chapters

--- web/static/js/components/Books.js
+++ web/static/js/components/Books.js
@@ -2,9 +2,31 @@ var React = require('react');
 var Book = require('./Book');
 
 var DATA = [
-  {id: 1, title: "Learning React"},
-  {id: 2, title: "Elm in Action"},
-  {id: 3, title: "Programming Elixir"},
+  {
+    id: 1,
+    title:
+    "Learning React",
+    chapters: [
+      {id: 1, title: "Chapter 1", done: true},
+      {id: 2, title: "Chapter 2", done: false}
+    ]
+  },
+  {
+    id: 2,
+    title: "Elm in Action",
+    chapters: [
+      {id: 1, title: "Chapter 1", done: true},
+      {id: 2, title: "Chapter 2", done: false}
+    ]
+  },
+  {
+    id: 3,
+    title: "Programming Elixir",
+    chapters: [
+      {id: 1, title: "Chapter 1", done: true},
+      {id: 2, title: "Chapter 2", done: false}
+    ]
+  },
 ]
 
 var Books = React.createClass({
